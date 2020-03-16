554c52ff02f3b2b29e36723925750be1c12ce8ba
ThibltC <thibault@fb-solutions.tech>
2020-03-16 13:45:18 +0100

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Test React FBSOLUTIONS</p>
+        </header>
+    )
+}
+
+export default Header
