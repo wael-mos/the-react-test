f31584a7ec49cbafc70792f79c32a3f0493adc7a
ThibltC <42961985+ThibltC@users.noreply.github.com>
2020-03-18 16:41:47 +0100

    Update Instructions.jsx

--- src/Instructions.jsx
+++ src/Instructions.jsx
@@ -17,7 +17,7 @@ const Insructions = () => {
                 <li>Le composant <span style={{ fontStyle: 'italic' }}>Infos</span> doit être enfant de <span style={{ fontStyle: 'italic' }}>Accueil</span>.</li>
                 <li>Créer un <span style={{ fontWeight: 'bold' }}>routing</span> pour passer de l'un à l'autre (avec des urls différentes) <a href="https://reacttraining.com/react-router/web/guides/quick-start" >(doc si nécessaire)</a></li>
                 <li>Dans <span style={{ fontStyle: 'italic' }}>Formulaire</span>, il faut un formulaire avec les champs <span style={{ fontWeight: 'bold' }}>nom</span>, <span style={{ fontWeight: 'bold' }}>prénom</span> et <span style={{ fontWeight: 'bold' }}>adresse</span>, puis un <span style={{ fontWeight: 'bold' }}>bouton</span> pour valider.</li>
-                <li>Afficher dans <span style={{ fontStyle: 'italic' }}>Infos</span> les informations validées par le formulaire.</li>
+                <li>Afficher les informations validées par le formulaire dans le composant <span style={{ fontStyle: 'italic' }}>Infos</span>.</li>
             </ul>
 
         </div>
